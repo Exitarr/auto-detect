3286dab070eab7927127d624b9249e56e602e8a2
Exitarr <souravdas212002@gmail.com>
2024-03-24 18:07:23 +0530

    final functionality completion

--- car_detection.py
+++ car_detection.py
@@ -8,16 +8,17 @@ def detect_cars(input_image_data):
     # Decode numpy array to image
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    
 
     # Load the pre-trained car classifier
     car_cascade = cv2.CascadeClassifier('static/cars.xml')
 
     # Convert the image to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gauss_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+
 
     # Detect cars in the image
-    cars = car_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    cars = car_cascade.detectMultiScale(gauss_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     #cars = car_cascade.detectMultiScale(gray_image, scaleFactor=1.05, minNeighbors=7, minSize=(60, 60))
 
     # Draw rectangles around the detected cars
